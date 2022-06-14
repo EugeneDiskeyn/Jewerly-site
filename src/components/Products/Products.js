5b89519510d4821f86239eff18aed0e4bb348149
Eugene <jdorovko@gmail.com>
2022-06-14 19:01:20 +0300

    Added utils

---
+++ src/components/Products/Products.js
@@ -0,0 +1,5 @@
+function Products (props) {
+    console.log(props.category);
+}
+
+export default Products;
---
+++ src/utils/catalog.js
@@ -0,0 +1,41 @@
+const catalog = {
+    "items": [
+        {
+            "id": 1,
+            "title": "Кольца",
+            "name": "rings"
+        },
+        {
+            "id": 2,
+            "title": "Серьги",
+            "name": "earrings"
+        },
+        {
+            "id": 3,
+            "title": "Цепочки",
+            "name": "chains"
+        },
+        {
+            "id": 4,
+            "title": "Кулоны",
+            "name": "pendants"
+        },
+        {
+            "id": 5,
+            "title": "Броши",
+            "name": "brooches"
+        },
+        {
+            "id": 6,
+            "title": "Подвески и колье",
+            "name": "necklace"
+        },
+        {
+            "id": 7,
+            "title": "Часы",
+            "name": "clocks"
+        }
+    ]
+}
+
+export default catalog;
